221a55a56a0fdcc1102b2071e91a8a6ce7cacb3b
kenshak <phylace0@gmail.com>
2021-08-18 15:30:00 +0200

    'no-offence'

--- src/app/tutor/student.controller.ts
+++ src/app/tutor/student.controller.ts
@@ -1,15 +1,16 @@
 import { Controller, Delete, Get, Post, Put, Param } from "@nestjs/common";
+import { FindStudentResponseDto, StudentResponseDto } from "../student/dto/student.dto";
 
 @Controller('tutors/:tutorId/students')
 export class StudentTutorController{
     @Get()
-    getTutorStudents(@Param('studentId') studentId:string){
+    getTutorStudents(@Param('studentId') studentId:string):FindStudentResponseDto[]{
         return 'Get tutor students'
     }
 
     @Put('/:studentId')
     updateTutorStudents(@Param('tutorId') tutorId:string,
-    @Param('studentId') studentId:string){
+    @Param('studentId') studentId:string):StudentResponseDto{
         return 'Update tutor students'
     }
 }
